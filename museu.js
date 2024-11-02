1ba0341e33c4fcb86d588c5e16736c76e0f749f0
Miguel <126696785+MiguelVascoDevWeb@users.noreply.github.com>
2024-11-02 15:18:30 -0300

    Scroll-view V1

---
+++ museu.js
@@ -0,0 +1,9 @@
+function scrollLeft() {
+    const container = document.getElementById("carrossel");
+    container.scrollBy(20, 20)
+}
+
+function scrollRight() {
+    const container = document.getElementById("carrossel");
+    container.scrollBy(20,20);
+}
